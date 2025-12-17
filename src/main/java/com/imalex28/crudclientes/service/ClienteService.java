08548ce9c4cba8ded11a72e6405401c5827eadf2
SOFTTEKPROJECTS\alejandro.gallegos <alejandro.gallegos@softtek.com>
2025-12-17 12:23:45 +0100

    Service layer basic implementation

--- src/main/java/com/imalex28/crudclientes/service/ClienteService.java
+++ src/main/java/com/imalex28/crudclientes/service/ClienteService.java
@@ -1,5 +1,56 @@
 package com.imalex28.crudclientes.service;
 
+import java.util.List;
+
+import com.imalex28.crudclientes.model.Cliente;
+import com.imalex28.crudclientes.repository.ClienteRepository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.NotFoundException;
+
+@ApplicationScoped
 public class ClienteService {
 
+    @Inject
+    ClienteRepository clienteRepository;
+
+    public List<Cliente> findAll() {
+    	// Llamamos al servicio de la capa Repositorio
+        return clienteRepository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        Cliente cliente = clienteRepository.findById(id);
+        if (cliente == null) {
+        	// Si el cliente no se encuentra
+            throw new NotFoundException("Cliente con ID " + id + " no encontrado");
+        }
+        return cliente;
+    }
+
+    public void save(Cliente cliente) {
+    	// Llamamos directamente al servicio de la capa Repo
+        clienteRepository.save(cliente);
+    }
+
+    public void update(Cliente cliente) {
+        Cliente existente = clienteRepository.findById(cliente.getIdCliente());
+        if (existente == null) {
+        	// Si el cliente no existe:
+            throw new NotFoundException("Cliente con ID " + cliente.getIdCliente() + " no encontrado");
+        }
+        
+        clienteRepository.update(cliente);
+    }
+
+    public void delete(Long id) {
+        Cliente existente = clienteRepository.findById(id);
+        if (existente == null) {
+        	// Si el cliente no existe:
+            throw new NotFoundException("Cliente con ID " + id + " no encontrado");
+        }
+        // Si existe, ya llamamos al delete
+        clienteRepository.delete(id);
+    }
 }
